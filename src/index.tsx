b18591197a8ba07fbac11f4932a6c8233ea00d1c
Yuvraj Dhakate <ydhakate@calibo.com>
2024-03-22 14:46:27 +0530

    updated template for nested path

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react'
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './home';
-import Dashboard from './dashboard-page';
-import Listing from './listing-page';
-import PageNotFound from './page-not-found';
 import './style.css';
+import Dashboard  from "./dashboard";
+import Listing from "./listing";
+import PageNotFound from './page-not-found'
 
 const App = () => {
   const basePath = process.env.REACT_APP_CONTEXT;
@@ -15,12 +15,15 @@ const App = () => {
           <nav style={{margin: '20px'}}>
               <Link to="/" style={{marginRight: '20px'}}>Home</Link>
               <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
-              <Link to="/listing">Listing</Link>
+              <Link to="/dashboard/listing">Listing</Link>
           </nav>
+
           <Routes>
+              <Route path="/dashboard/*">
+                    <Route index element={<Dashboard />} />
+                    <Route path='listing' element={<Listing />} />
+              </Route>
               <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/listing" element={<Listing />} />
               <Route path="*" element={<PageNotFound />} />
           </Routes>
       </div>
